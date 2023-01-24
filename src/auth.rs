05a7590959054f4459eaf35c63415bb94c4e0b97
Richard McHorgh <mchorghr2@gmail.com>
2023-01-24 14:57:09 -0500

    authentication works

--- src/auth.rs
+++ src/auth.rs
@@ -2,19 +2,26 @@ extern crate dirs;
 extern crate rocket;
 extern crate urlencoding;
 
+use rocket::http::uri::{Origin, Reference};
 use rocket::response::content::RawHtml;
 use rocket::{build, custom, Config, Shutdown, Request};
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 use std::fs::read_to_string;
 use std::process::Command;
 use urlencoding::encode;
 
 use crate::constants::{CLIENT_ID, REDIRECT, SCOPES};
 
-pub(crate) fn key() -> Result<String, Box<dyn std::error::Error>> {
-    let mut f = read_to_string(format!(
+fn keyPath() -> String {
+    format!(
         "{}/.config/spottybar/key",
         dirs::home_dir().unwrap().display()
-    ))
+    )
+}
+
+pub(crate) fn key() -> Result<String, Box<dyn std::error::Error>> {
+    let mut f = read_to_string(keyPath())
     .expect("Couldn't get key.");
 
     if f.contains("\n") {
@@ -39,10 +46,35 @@ pub(crate) fn authLink() {
 }
 
 #[get("/token")]
-pub(crate) async fn token(t: Token, shutdown: Shutdown) -> &'static str {
-    println!("fragment: {}", t);
+pub(crate) async fn tokenPage() -> RawHtml<&'static str> {
+    RawHtml(r#"
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Spotify Web API</title>
+        </head>
+        <body>
+            <p>Recieved</p>
+            <script>
+                fetch(`http://localhost:3000/token/${window.location.hash.substr(14, window.location.hash.length - 48)}`, {
+                    method: 'POST',
+                    body: window.location.hash.substr(14, window.location.hash.length - 48)
+                })
+                .catch(console.error)
+            </script>
+        </body>
+    </html>
+    "#)
+}
+
+#[post("/token/<token>")]
+pub(crate) async fn saveToken(token: String, shutdown: Shutdown) -> &'static str {
+    println!("{}", token);
+    let mut f = File::create(keyPath()).await.expect("Should've created a file.");
+    f.write_all(token.as_bytes()).await.expect("Couldn't write to file.");
+
     shutdown.notify();
-    "auth page"
+    "saved token"
 }
 
 #[launch]
@@ -51,5 +83,6 @@ pub(crate) fn rocket() -> _ {
         port: 3000,
         ..Config::default()
     };
-    custom(&c).mount("/", routes![token])
+    custom(&c).mount("/", routes![tokenPage])
+    .mount("/", routes![saveToken])
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,8 +1,8 @@
 use rocket::{
-    data::Outcome,
-    http::{uri::Reference, Status},
-    request::FromRequest,
-    Request,
+    data::{FromData, Outcome},
+    http::{ext::IntoOwned, uri::Reference, Status},
+    request::{self, FromRequest, Request},
+    Data,
 };
 use snafu::prelude::*;
 use std::convert::{Infallible, TryFrom};
@@ -71,29 +71,7 @@ pub(crate) enum SpottyBarError {
 
     #[snafu(display("Request Error"))]
     RequestError,
-}
 
-struct ApiKey<'r>(&'r str);
-#[derive(Debug)]
-enum ApiKeyError {
-    Missing,
-    Invalid,
-}
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for ApiKey<'r> {
-    type Error = ApiKeyError;
-
-    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        /// Returns true if `key` is a valid API key string.
-        fn is_valid(key: &str) -> bool {
-            key == "valid_api_key"
-        }
-
-        match req.headers().get_one("x-api-key") {
-            None => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
-            Some(key) if is_valid(key) => Outcome::Success(ApiKey(key)),
-            Some(_) => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid)),
-        }
-    }
+    #[snafu(display("Post Request Error"))]
+    PostRequestError,
 }
